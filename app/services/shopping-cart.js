17413bef1cbc30ea1778afe8c32e37b49315146c
Gracie Lundell and Kyle Derrick <kderrick1988@gmail.com>
2016-04-06 13:32:34 -0700

    get total price of all items in the cart

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  totalCart: Ember.computed('items.[]', function() {
+    var totalCost = 0;
+    var items = this.get('items');
+    console.log("items: ", items);
+    for (var i = 0; i < items.length; i++ ) {
+      totalCost += items[i].get('price');
+    }
+    return totalCost;
+  }),
 
   add(item) {
     this.get('items').pushObject(item);
   }
+
 });
