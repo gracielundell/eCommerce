52ecb695e9b56ed8a48f83b447274430c53eb281
Gracie Lundell and Kyle Derrick <kderrick1988@gmail.com>
2016-04-07 15:34:43 -0700

    add avaraging of rating, but not working after a page refresh

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -1,10 +1,46 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // productRating: Ember.computed('comment.rating')
+  productRating: Ember.computed('product.comments.length', function() {
+   var i = 0;
+   var j = 0;
+   this.product.get('comments').map(function(comment){
+    //  debugger;
+     j += 1;
+     i += parseInt(comment.get('rating'));
+   });
+    return (i / j);
+ }),
+  // productRating: Ember.computed('product.comments', function() {
+  //   // var averageRating = this.get('comment').then(function(stuff){
+  //   //
+  //   //   debugger;
+  //   // });
+  //   // debugger;
+  //   this.get('product').get('comments').map(function(comment){
+  //     // debugger;
+  //     console.log(comment.get('rating'));
+  //     // return product.get('comments');
+  //   })
+  //
+  //   var totalRating = 0;
+  //   // comments.forEach(function(comment){
+  //   //   console.log(comment.get('author'));
+  //   //   totalRating += parseInt(comment.get('rating'));
+  //   // })
+  //   // return totalRating / (comments.length);
+  //   // debugger;
+  //   //averageRating.get('content').currentState[0] //works with {}
+  //   // console.log("average rating", averageRating);
+  //   // return averageRating;
+  // }),
   actions: {
     deleteComment(comment) {
       this.sendAction('deleteComment', comment);
     }
   }
 });
+// var totalRating = 0
+// for(var i = 0;i < arrayName.length; i++) {
+//   var totalRating += arrayName[i].rating
+// } return totalRating/arrayName.length;
